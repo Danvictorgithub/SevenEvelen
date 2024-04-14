22421525bef9ec095ef27b7bca0f6207b7f4733c
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2024-04-14 18:47:12 +0800

    Enabled Cors, Helmet and Logger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import helmet from 'helmet';
+import * as logger from "morgan";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.use(helmet());
+  app.use(logger('dev'));
   await app.listen(8080);
 }
 bootstrap();
